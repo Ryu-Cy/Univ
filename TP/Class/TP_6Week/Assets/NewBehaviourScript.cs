72f066e7566259d48753eb36475c6ea228fb7812
RyuCY <rcy1235@naver.com>
2021-05-10 22:57:24 +0900

    2021.05.10
    TP_Class_6, 8

---
+++ TP/Class/TP_6Week/Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    protected Animator ani;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        ani.SetFloat("Speed1", 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ani.SetFloat("Speed1", 1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ani.SetFloat("Speed1", 2.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ani.SetFloat("Speed1", 3.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ani.SetFloat("Speed1", 4.0f);
+        }
+    }
+}
---
+++ TP/Class/TP_8Week/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    bool bCheck_Parameter = true;
+
+    int bHardWare = 0;
+
+    [SerializeField] private InputField Name_Input;
+    [SerializeField] private InputField PassWord_Input;
+
+    [SerializeField] private Slider Slider_Value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MyButtonClick()
+    {
+        Debug.Log("Button Click");
+    }
+    public void MyCheckBoxClick()
+    {
+        bCheck_Parameter = !bCheck_Parameter;
+        Debug.Log("Check: " + bCheck_Parameter);
+    }
+    public void MyDxRadioButtonClick()
+    {
+        bHardWare = 1;
+        Debug.Log("Select: Dx" + bHardWare);
+    }
+    public void MyGLRadioButtonClick()
+    {
+        bHardWare = 2;
+        Debug.Log("Select: OpenGL" + bHardWare);
+    }
+    public void MyInputField_Name()
+    {
+        if (Name_Input.text == "Ryu")
+            Debug.Log("Ok");
+    }
+    public void MyInputField_PassWord()
+    {
+        if (PassWord_Input.text == "1576")
+            Debug.Log("Ok");
+    }
+    public void MySlider()
+    {
+        Debug.Log("Slider Value: " + Slider_Value.value);
+    }
+}
